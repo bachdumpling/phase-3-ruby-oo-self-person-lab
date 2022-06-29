24ace4e6937cd1126b60aecea97ab81ee17e1df4
Bach Le <lehoangbach7802@gmail.com>
2022-06-29 17:18:19 -0400

    commit tree 61c4771f75a2194c13fe1bf5868d4e34a7d1ea2d

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,86 @@
-# your code goes here
+require "pry"
+
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+
+    def initialize (name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+        
+    end
+
+    def happiness= (num)
+        if num > 10 
+            @happiness = 10
+        elsif num < 0
+            @happiness = 0
+        else 
+            @happiness = num
+        end
+    end
+
+    def hygiene= (num)
+        if num > 10
+            @hygiene = 10
+        elsif num < 0
+            @hygiene = 0
+        else
+            @hygiene = num
+        end
+    end
+
+    def happy?
+        if self.happiness > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def clean?
+        if self.hygiene > 7
+            return true
+        else
+            return false
+        end
+    end
+
+    def get_paid(amount)
+        self.bank_account += amount.to_i
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+end
